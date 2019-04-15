b7f6377261e08713c441eaba0fcb711ccb170666
Nathan Shull <nshull16@iastate.edu>
2019-04-14 21:35:46 -0500

    getting some methods started

--- pa2/src/CommunicationsMonitor.java
+++ pa2/src/CommunicationsMonitor.java
@@ -1,4 +1,41 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
 
 public class CommunicationsMonitor {
+	
+	private HashMap<Integer, List<ComputerNode>> map = new HashMap<Integer, List<ComputerNode>>();
+	
 
+	public CommunicationsMonitor(){
+		
+	}
+	
+	public void addCommunication(int c1, int c2, int timestamp){
+		ComputerNode nodec1 = new ComputerNode(c1, timestamp);
+		ComputerNode nodec2 = new ComputerNode(c2, timestamp);
+		nodec1.addOutNeighbor(nodec1);
+		nodec2.addOutNeighbor(nodec2);
+		map.get(c1).add(nodec1);
+		map.get(c2).add(nodec2);
+	}
+	
+	public void createGraph(){
+		
+	}
+	
+	public List<ComputerNode> queryInfection(int c1, int c2, int x, int y){
+		List<ComputerNode> cnlist = new ArrayList<ComputerNode>();
+		return cnlist;
+	}
+	
+	public HashMap<Integer, List<ComputerNode>> getComputerMapping(){
+		return this.map;
+	}
+	
+	public List<ComputerNode> getComputerMapping(int c){
+		return this.map.get(c);
+	}
+	
+	
 }
--- pa2/src/ComputerNode.java
+++ pa2/src/ComputerNode.java
@@ -1,4 +1,27 @@
+import java.util.List;
 
 public class ComputerNode {
-
+	
+	private int id;
+	private int timestamp;
+	private List<ComputerNode> neighbors;
+	public ComputerNode(int id, int timestamp){
+		this.id = id;
+		this.timestamp = timestamp;
+	}
+	
+	public int getID(){
+		return this.id;
+	}
+	
+	public int getTimeStamp(){
+		return this.timestamp;
+	}
+	public List<ComputerNode> getOutNeighbors(){
+		return this.neighbors;
+	}
+	
+	public void addOutNeighbor(ComputerNode outNeighbor){
+		neighbors.add(outNeighbor);
+	}
 }
